d7d4b96027c15e9e2cf1e6d0cf581a16fbc34e1c
esantix <santiago93echevarria@gmail.com>
2024-08-26 10:33:28 -0300

    Spell check on commit message

--- set_global.sh
+++ set_global.sh
@@ -3,7 +3,7 @@
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 HOOKS_PATH=$SCRIPTPATH/hooks
 
-chmod +x $HOOKS_PATH/
+chmod +x $HOOKS_PATH/*
 
 echo "Setting global hooks from $HOOKS_PATH"
 git config --global core.hooksPath $HOOKS_PATH
